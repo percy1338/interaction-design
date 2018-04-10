82e86d219af02946cacd0a6b5545f446fb901949
Tiemen Kamp <t.kamp@sintlucasedu.nl>
2018-04-10 20:43:44 +0200

    Implemented HUD with working weapon wheel, created a simple level as game background

--- tryagain/Assets/scripts/Game.cs
+++ tryagain/Assets/scripts/Game.cs
@@ -5,9 +5,20 @@ using UnityEngine;
 public class Game : MonoBehaviour
 {
     public GameObject gameMenu;
+    public GameObject optionsMenu;
     public GameObject gameSettings;
     public GameObject videoSettings;
     public GameObject audioSettings;
+
+    public GameObject pauseHeader;
+
+    public WeaponWheel weaponWheel;
+    public GameObject crossHair;
+    public GameObject HUD;
+
+    public GameObject HalfHealth;
+
+    private bool _paused;
 
 	// Use this for initialization
 	void Start ()
@@ -23,28 +34,62 @@ public class Game : MonoBehaviour
     {
 		if(Input.GetKeyDown(KeyCode.Escape))
         {
-            if(gameMenu.active == false)
+            if(!_paused)
             {
                 OpenMenu();
+                weaponWheel.ToggleWheel(false);
+                crossHair.SetActive(true);
             }
             else
             {
                 CloseMenu();
             }
         }
-	}
+        if (!_paused) {
+            if (Input.GetKeyDown(KeyCode.Mouse2))
+            {
+                weaponWheel.ToggleWheel(true);
+                crossHair.SetActive(false);
+            }
+            else if (Input.GetKeyUp(KeyCode.Mouse2))
+            {
+                weaponWheel.ToggleWheel(false);
+                crossHair.SetActive(true);
+            }
+
+            if (Input.GetKeyDown(KeyCode.H))
+            {
+                HalfHealth.SetActive(!HalfHealth.activeSelf);
+            }
+        }
+    }
 
     public void OpenMenu()
     {
         gameMenu.SetActive(true);
+        optionsMenu.SetActive(false);
         gameSettings.SetActive(false);
         videoSettings.SetActive(false);
         audioSettings.SetActive(false);
+        _paused = true;
+        pauseHeader.SetActive(true);
     }
 
     public void CloseMenu()
     {
         gameMenu.SetActive(false);
+        optionsMenu.SetActive(false);
+        gameSettings.SetActive(false);
+        videoSettings.SetActive(false);
+        audioSettings.SetActive(false);
+        _paused = false;
+        pauseHeader.SetActive(false);
+    }
+
+    public void OptionsMenu()
+    {
+        gameMenu.SetActive(false);
+        optionsMenu.SetActive(true);
         gameSettings.SetActive(false);
         videoSettings.SetActive(false);
         audioSettings.SetActive(false);
@@ -53,6 +98,7 @@ public class Game : MonoBehaviour
     public void GameSettings()
     {
         gameMenu.SetActive(false);
+        optionsMenu.SetActive(false);
         gameSettings.SetActive(true);
         videoSettings.SetActive(false);
         audioSettings.SetActive(false);
@@ -60,17 +106,31 @@ public class Game : MonoBehaviour
     public void VideoSettings()
     {
         gameMenu.SetActive(false);
+        optionsMenu.SetActive(false);
         gameSettings.SetActive(false);
-        videoSettings.SetActive(false);
+        videoSettings.SetActive(true);
         audioSettings.SetActive(false);
     }
 
     public void AudioSettings()
     {
         gameMenu.SetActive(false);
+        optionsMenu.SetActive(false);
         gameSettings.SetActive(false);
         videoSettings.SetActive(false);
         audioSettings.SetActive(true);
+    }
+
+    public void BackToMain()
+    {
+        Application.LoadLevel(0);
+        Debug.Log("start game");
+    }
+
+    public void RestartLevel()
+    {
+        Application.LoadLevel(1);
+        Debug.Log("start game");
     }
 
 }
---
+++ tryagain/Assets/scripts/WeaponWheel.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponWheel : MonoBehaviour {
+
+    public GameObject[] WheelComponents;
+    private bool selecting = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (selecting) {
+            Vector3 mousePos = Input.mousePosition;
+            GameObject closestComponent = WheelComponents[0];
+
+            for (int i = 0; i < WheelComponents.Length; i++)
+            {
+                WheelComponents[i].transform.localScale = new Vector3(1, 1, 1);
+                if ((WheelComponents[i].transform.position - mousePos).magnitude < (closestComponent.transform.position - mousePos).magnitude)
+                {
+                    closestComponent = WheelComponents[i];
+                }
+            }
+            closestComponent.transform.localScale = new Vector3(1.25f, 1.25f, 1.25f);
+        }
+	}
+
+    public void ToggleWheel(bool toggle)
+    {
+        if (toggle)
+        {
+            selecting = true;
+            for (int i = 0; i < WheelComponents.Length; i++)
+            {
+                WheelComponents[i].SetActive(true);
+            }
+        }
+        else
+        {
+            selecting = false;
+            for (int i = 0; i < WheelComponents.Length; i++)
+            {
+                WheelComponents[i].SetActive(false);
+            }
+        }
+    }
+}
